3f35a8c33962b96b9730e71ef5c24939d01c20ca
Isabella18081996 <Isabella.Nita@yahoo.it>
2021-04-06 15:13:02 +0200

    esercizio finito

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var mionome = prompt('Quale è il tuo nome?');
+var miocognome = prompt('Quale è il tuo cognome?');
+var colorepreferito = prompt('Quale è il tuo colore preferito?');
+
+document.getElementById('password').innerHTML = mionome + miocognome + colorepreferito + "21"
